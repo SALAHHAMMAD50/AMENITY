d76b5cdeb6b49916d3b2fb2b5f4ba70afd15e7cd
SALAHHAMMAD50 <83323563+SALAHHAMMAD50@users.noreply.github.com>
2023-04-17 00:59:28 +0200

    salah

---
+++ app/src/main/java/com/salahFH/amenity/SplashScreen.kt
@@ -0,0 +1,17 @@
+package com.salahFH.amenity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
